c95f7b0faa9cb0931effb689d857ddc40d4f0968
robiul0278 <robiul0278@gmail.com>
2023-05-18 00:14:56 +0600

    basic route setup complete

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      errorElement: <ErrorPage></ErrorPage>,
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+
+  export default router;
